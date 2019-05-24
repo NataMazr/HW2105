6c229cf473cd3ac8817ca7f7806fa6dc978627ac
Nata <generalmnf@gmail.com>
2019-05-24 16:55:56 +0300

    дополнение

--- main.js
+++ main.js
@@ -7,7 +7,37 @@ for (i = 0; i < arr.length - 1; i++) {
     } else if (arr[i] > max) {
         max = arr[i];
     }
-
 }
 console.log(min);
 console.log(max);
+
+let mas = [
+    11,
+    5.5,
+    "строка1",
+    25,
+    "строка2",
+    true,
+    9.81, [2, 34, 7],
+    10
+];
+let result1 = [];
+let result2 = [];
+let result3 = [];
+let result4 = [];
+
+for (let j = 0; j < mas.length; j++) {
+    if (mas[j] === "boolean") {
+        result1.push(mas[j]);
+    } else if (mas[j] === "object") {
+        result2.push(mas[j]);
+    } else if (mas[j] === "number") {
+        result3.push(mas[j]);
+    } else if (mas[j] === "string") {
+        result4.push(mas[j]);
+    }
+}
+console.log(result1);
+console.log(result2);
+console.log(result3);
+console.log(result4);
